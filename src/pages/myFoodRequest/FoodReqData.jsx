ad6ff7ef4e29e80ad11cdce51b389e29d26cf725
emonhasan17 <emon1.798045@gmail.com>
2025-06-26 02:02:54 +0600

    fix card

--- src/pages/myFoodRequest/FoodReqData.jsx
+++ src/pages/myFoodRequest/FoodReqData.jsx
@@ -12,7 +12,7 @@ const FoodReqData = ({ foodReq }) => {
                 <h1 className='text-5xl md:text-6xl font-semibold text-[#FCBC58]'>My Requested Meals</h1>
                 <p className='text-xl text-[#38B4D0]'>See all the food you’ve requested from kind-hearted donors.</p>
             </div>
-            <div className=' md:max-w-8/12 mx-auto'>
+            <div className='w-11/12 md:max-w-8/12 mx-auto'>
                 {
                     foodReq.map(req => <FoodReqCard key={req._id} req={req}></FoodReqCard>)
                 }
